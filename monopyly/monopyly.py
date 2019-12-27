07ff55e2a7e563f92ae6458b74cc3951c4cedc2d
Jason Fong <jason.fong@shopify.com>
2019-12-27 18:25:45 -0500

    updates

--- monopyly/monopyly.py
+++ monopyly/monopyly.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-class Monopyly(object):
+class Blocks(object):
 
 	def __init__(self, property_value, purchase_price, interest_rate, down_payment_pct, loan_term, rental_income, 
 				 vacancy=0, maintenance_reserve=0, management_fee=0, monthly_property_tax=0, monthly_insurance=0,
@@ -94,10 +94,31 @@ class Monopyly(object):
 	def total_operating_expense(self):
 		return self.property_tax() + self.insurance() + self.utility() + self.property_management_amt()
 
+	def effective_net_cash_flow(self):
+		return self.annual_cash_flow() + self.principal_paydown()
+
+	def accumulated_cash_flow(self):
+		return np.cumsum(self.annual_cash_flow())
+
 	# Equity Accumulation
 
 	def equity_at_purchase(self): # DOUBLE CHECK
 		return self.property_value - self.purchase_price + self.down_payment_amt
+
+	def mortgage_balance(self):
+		return np.array([round(np.fv(self.interest_rate / 12, i*12, self.monthly_payments(), -self.loan_amount()), 2) for i in self._generate_years()])
+
+	def principal_paydown(self):
+		return np.array([self._cumulative_principal(i*12-11, i*12, self.interest_rate, self.loan_term, self.loan_amount()) for i in self._generate_years()])
+
+	def equity_wealth(self):
+		return self.property_value - self.mortgage_balance()
+
+	def plus_sales_proceeds_if_final_year(self):
+		return self.annual_cash_flow() + self.equity_wealth() - self.property_value*self.closing_cost_sell
+
+	def effective_future_value(self):
+		return self.plus_sales_proceeds_if_final_year() + self.accumulated_cash_flow() + self.annual_cash_flow()
 
 	# Financial Performance
 
@@ -107,7 +128,30 @@ class Monopyly(object):
 	def cash_on_cash_return(self):
 		return np.round((self.annual_cash_flow() / self.total_cash_required())*100, 2)
 
-	def principal_paydown(self):
-		return np.array([self._cumulative_principal(i*12-11, i*12, self.interest_rate, self.loan_term, self.loan_amount()) for i in self._generate_years()])
+	def return_on_equity(self):
+		return self.effective_net_cash_flow() / self.equity_wealth()
+
+	def annualized_return(self):
+		annualized_return = self.effective_future_value() / self.total_cash_required()
+		for i in range(0,len(annualized_return)):
+			annualized_return[i] = annualized_return[i]**(1/(i+1))-1
+		return annualized_return
+
+	def return_on_investment(self):
+		return (self.plus_sales_proceeds_if_final_year() / self.total_cash_required()) / self.total_cash_required()
+
+	def print_period_sampler(self):
+		return list(range(1,11)) + [10, 20, 30]
+
+	def print_financial_performance(self, full_period=False):
+		financial_performance_df = pd.DataFrame({
+			'Year': [i for i in self._generate_years()],
+			'Cash on Cash COC': self.cash_on_cash_return(),
+			'Return on Equity ROE': self.return_on_equity(),
+			'Annualized Return APY': self.annualized_return(),
+			'Return On Investment': self.return_on_investment()
+		})
+
+		return financial_performance_df.set_index('Year').T
 
 	# have the ability to print financial statement?
